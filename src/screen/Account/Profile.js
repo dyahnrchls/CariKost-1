31acee1dfb95ad60ec6dcfe57ccf93a790d60bf1
dyah <dyahnrchls@gmail.com>
2019-08-17 23:44:20 +0700

    modified profile screen

--- src/screen/Account/Profile.js
+++ src/screen/Account/Profile.js
@@ -2,65 +2,75 @@ import React, { Component } from 'react';
 import { StyleSheet, TouchableOpacity, Text, Image, View, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+const dataProfile = [
+    {
+        icon_name: 'ios-home',
+        text: 'Houses'
+    },
+    {
+        icon_name: 'ios-paper',
+        text: 'Booking List'
+    },
+    {
+        icon_name: 'ios-images',
+        text: 'Ads'
+    },
+    {
+        icon_name: 'ios-create',
+        text: 'Account Verification'
+    },
+    {
+        icon_name: 'ios-settings',
+        text: 'Settings'
+    },
+    {
+        icon_name: 'ios-call',
+        text: 'Call Center'
+    },
+]
+
 export default class Profile extends Component {
+
+
     render() {
         return (
-            <ScrollView style={{ backgroundColor: '#dfe6e9' }}>
-                <View style={{ flexDirection: 'row', marginLeft: 20, marginTop: 20, justifyContent: 'space-between', paddingRight: 20, paddingLeft: 20 }}>
+            <ScrollView>
+                <View style={{ flexDirection: 'row', paddingLeft: 20, paddingTop: 20, justifyContent: 'space-between', paddingRight: 20, paddingLeft: 20, }}>
                     <View style={{ justifyContent: 'flex-start', flexDirection: 'row' }}>
-                        <Icon name='ios-person' size={40} color='#00a663' />
-                        <View style={{ flexDirection: 'column' }}>
+                        <Icon name='ios-person' size={40} />
+                        <View style={{ flexDirection: 'column', paddingLeft: 10 }}>
                             <Text style={styles.Text}>Dyah</Text>
                             <Text style={styles.Text}>999786</Text>
                         </View>
-
                     </View>
+                    <TouchableOpacity onPress={this.editProfile}><Text style={{ color: 'blue' }}>Edit Profile</Text></TouchableOpacity>
+                </View>
 
-                    <TouchableOpacity onPress={this.editProfile}><Text>Edit Profile</Text></TouchableOpacity>
-                </View>
-                <TouchableOpacity style={styles.card}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Icon name='ios-home' size={40} color='#00a663' />
-                        <Text style={styles.Text}>Kost Saya</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('BookingPage')} style={styles.card}>
-                    <Icon name="ios-paper" size={30} color='#00a663' />
-                    <Text style={styles.Text}>Booking List</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.card} onPress={() => this.props.navigation.navigate('Iklan')}>
-                    <Icon name="ios-images" size={30} color='#00a663' />
-                    <Text style={styles.Text}>Data Iklan</Text>
-                </TouchableOpacity>
-                <View style={styles.card}>
-                    <Icon name="ios-create" size={30} color='#00a663' />
-                    <Text style={styles.Text}>Account Verification </Text>
-                </View>
-                <View style={styles.card}>
-                    <Icon name="ios-settings" size={30} color='#00a663' />
-                    <Text style={styles.Text}>Pengaturan</Text>
-                </View>
-                <TouchableOpacity style={styles.card} >
-                    <Icon name='ios-call' size={30} color='#00a663' />
-                    <Text style={styles.Text}>Hubungi CS</Text>
-                </TouchableOpacity>
+                {dataProfile.map((list, index) => {
+                    return (
+                        <TouchableOpacity key={index} style={styles.card}>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Icon name={list.icon_name} size={40} color='#00a663' />
+                                <Text style={styles.Text}>{list.text}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                }
+                )}
             </ScrollView>
         )
     }
 
-    pressButton = () => {
-        this.props.navigation.navigate('Login')
-    }
-
     editProfile = () => {
-        alert('coming soon');
+        this.props.navigation.navigate('EditProfile')
     }
 }
 
 const styles = StyleSheet.create({
     Text: {
         fontSize: 15,
-        marginLeft: 10
+        paddingLeft: 10,
+        alignContent: 'center'
     },
     card: {
         backgroundColor: 'white',
